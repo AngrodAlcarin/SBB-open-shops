383fda1cecc390b546b37272b8b769efb1cfa3f2
Angrod <mare.imholz@sunrise.ch>
2023-08-07 15:57:22 +0200

    added custom markers, reactive markers based on selection

--- SBB-open-shops/app.R
+++ SBB-open-shops/app.R
@@ -9,70 +9,62 @@
 ##install.packages("shinyDatetimePickers")
 ##install.packages("DT")
 ##install.packages("leaflet")
+##install.packages("shinydashboard")
 
 library(shiny)
 library(shinyDatetimePickers)
+library(shinydashboard)
 library(DT)
 library(leaflet)
 
+
 # Define UI for application that draws a histogram
+
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("SBB Open Shops"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput(inputId="station_selector",
-                        label="Train Station",
-                        choices=sort(unique(sbbshops$Haltestellen.Name))
-                        ),
-            datetimeMaterialPickerInput(inputId="Time",label="Choose time", value = NULL, style = NULL)
-        ),
-        
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          h3(textOutput("table_title")),
-          leafletOutput("map"),
-          dataTableOutput("shops_output")
-        )
+  titlePanel("SBB Open Shops"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(
+        inputId = "station_selector",
+        label = "Train Station",
+        choices = sort(unique(sbbshops$Haltestellen.Name))
+      ),
+      datetimeMaterialPickerInput(
+        inputId = "Time",
+        label = "Choose time",
+        value = NULL,
+        style = NULL
+      )
+    ),
+    
+    mainPanel(
+      h3(textOutput("table_title")),
+      leafletOutput("map", height = 350),
+      div(
+        style = "height: 600px; width: 100%; overflow-y: auto;",
+        dataTableOutput("shops_output")
+      )
+      
     )
+  )
 )
 
-# Define server logic required to draw a histogram
+
+
 server <- function(input, output) {
   
+  #the title above the map and table, reactive on which station is selected
   output$table_title <- renderText({
     station <- input$station_selector
     if (!is.null(station)) {
       paste("Shops at", station, "station")
     }
   })
+
   
-  output$map <- renderLeaflet({
-    station <- input$station_selector
-    
-    if (!is.null(station)) {
-      filtered_data <- sbbshops[sbbshops$Haltestellen.Name == station, ]
-      
-      map <- leaflet() %>%
-        addTiles()  # Add default map tiles
-      
-      for (i in 1:nrow(filtered_data)) {
-        lat <- as.numeric(unlist(strsplit(filtered_data$Geoposition[i], ", "))[1])
-        lon <- as.numeric(unlist(strsplit(filtered_data$Geoposition[i], ", "))[2])
-        name <- filtered_data$Name[i]
-        
-        map <- addMarkers(map, lat = lat, lng = lon, popup = name)
-      }
-      
-      map
-    }
-  })
-  
-  
+  #data table containing all shops at the selected train station
   output$shops_output <- renderDataTable({
     station <- input$station_selector
     if (!is.null(station)) {
@@ -80,30 +72,88 @@ server <- function(input, output) {
       shops_list <- filtered_data$Name
       logos_list<-filtered_data$logourl2
       loc_list<-filtered_data$name_affix
+      cat_list<-filtered_data$category
       if (length(shops_list) > 0) {
         shops_df <- data.frame(Shop_Logos =logos_list,
                                Shop_Names = shops_list,
-                               Shop_Names=loc_list)
+                               Shop_Names=loc_list,
+                               Shop_Names=cat_list)
+    
+    #reactive expression to highlight selected shop(s) on the map    
+    selected_shop <- reactiveVal(NULL)
+    
+    #leaflet map with markers on all the shops at the selected train station.
+    output$map <- renderLeaflet({
+      station <- input$station_selector
+      
+      #observer for debugging selecting rows  
+      observeEvent(input$shops_output_rows_selected, {
+        selected_row <- input$shops_output_rows_selected
+        if (length(selected_row) > 0) {
+          selected_shop_name <- filtered_data[selected_row, "Name"]
+          selected_shop(selected_shop_name)
+          
+          # Print the selected row index and corresponding shop name
+          cat("Selected Row Index:", selected_row, "\n")
+          cat("Selected Shop Name:", selected_shop_name, "\n")
+        } else {
+          selected_shop(NULL)  # Remove the selected shop name
+        }
+          
+          
+        })
+    
+    
+      
+      if (!is.null(station)) {
+        filtered_data <- sbbshops[sbbshops$Haltestellen.Name == station, ]
         
+        map <- leaflet() %>%
+          addTiles()  # Add default map tiles
+        #make selected shops red and bigger highlighted
+        selected_rows <- input$shops_output_rows_selected
+        #unlist the latitude and longitude from the dataset, it was in a single column
+        for (i in 1:nrow(filtered_data)) {
+          lat <- as.numeric(unlist(strsplit(filtered_data$Geoposition[i], ", "))[1])
+          lon <- as.numeric(unlist(strsplit(filtered_data$Geoposition[i], ", "))[2])
+          name <- filtered_data$Name[i]
+          #if row is selected, show it as red and bigger
+          if (i %in% selected_rows) {
+            icon_url <- "pin-l-shop+f00.png"
+            icon <- makeIcon(iconUrl = icon_url, iconWidth = 45, iconHeight = 112.5)
+          } 
+          #if not, show it as a normal blue smaller pin
+          else {
+            icon_url <- "pin-l-shop+12a.png"
+            icon <- makeIcon(iconUrl = icon_url, iconWidth = 35, iconHeight = 87.5)
+          }
+          
+          map <- addMarkers(map, lat = lat, lng = lon, popup = name, icon = icon)
+        }
         
+        map
+      }
+    })
+    
+        #the table itself
         datatable(
           shops_df,
-          escape=FALSE,
-          colnames = c("","Shops","Location"),
+          escape=FALSE, #allowing html for the images
+          colnames = c("","Shop","Location", "Category"),
           options = list(
-            pageLength = 25,
+            pageLength = 10,
             lengthChange = FALSE,
             lengthMenu = list(c(-1), c("All")),
             pagingType = "numbers",
             columnDefs = list(
               list(
-                targets = c(0,2),  # Index of the column (0-based) you want to disable sorting for
+                targets = c(0,2),  # disable sorting for location and logo
                 orderable = FALSE
               )
             ),
-            order = list(list(1, 'asc')) 
+            order = list(list(1, 'asc')) #list alphabetically by the shop name
           ),
-          rownames = FALSE
+          rownames = FALSE#remove the numbering of the shops
         )
       }
     }

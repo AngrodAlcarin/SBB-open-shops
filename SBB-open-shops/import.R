4039d09d1842057535b839a37cbd339908529c9c
Marius Imholz <mare.imholz@sunrise.ch>
2023-10-17 10:10:12 +0200

    changed import function (SBB changed data source and format)

--- SBB-open-shops/import.R
+++ SBB-open-shops/import.R
@@ -8,8 +8,9 @@ library(lubridate)
 sbbopenshops<-read.csv(url("https://data.sbb.ch/api/explore/v2.1/catalog/datasets/offnungszeiten-shops/exports/csv?limit=-1&lang=de&timezone=UTC&use_labels=true&epsg=4326"), sep = ';')
 
 sbbshops<-sbbopenshops %>% 
-  select(c(1,5,7:13,17,34:37)) %>% 
-  ##filter(!category %in% c("Öffentlicher Verkehr","Piktogramm (Übrige)", "Piktogramm SBB Schalter")) %>% 
+  select(c(2,3,13,18,19,25,26,29,36,46,47))
+
+sbbshops<-sbbshops %>% 
   filter(!category %in% c("Öffentlicher Verkehr","Piktogramm (Übrige)")) %>% 
   mutate(logourl=paste("https://stations.sbb.cartaro-enterprise.com", logo,sep = ""),
          logourl2=paste("<img src=", "\"", logourl, "\""," width=\"80\"></img>",sep=""),
